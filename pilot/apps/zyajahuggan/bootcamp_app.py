65a614984b0330c4579e3b25acac066fa52b3c61
Zyaja Huggan <zyhuggan@MacBook-Pro-10.local>
2025-11-04 17:32:41 -0600

    Secondary Structure identify_secondary_structure_spans handler

--- pilot/apps/zyajahuggan/bootcamp_app.py
+++ pilot/apps/zyajahuggan/bootcamp_app.py
@@ -6,9 +6,8 @@ from rosetta.core.pack.task import operation
 from rosetta.core.pack.task import TaskFactory
 from rosetta.core.pack import pack_rotamers
 from rosetta.core import kinematics, optimization
-from rosetta.core.pack.task import operation
 from pyrosetta.rosetta.protocols.moves import AddPyMOLObserver, MonteCarlo
-
+'''
 init(extra_options="-ignore_unrecognized_res")
 
 # Initialize parser
@@ -48,8 +47,11 @@ the_observer = AddPyMOLObserver(mypose)
 the_observer.pymol().apply(mypose)
 
 # Monte Carlo Loop 
+counter_True = 0
+counter_False = 0
 total =  mypose.total_residue()
-for i in range(50):
+
+for i in range(10):
     pack_rotamers(mypose,sfxn,task)
     minimizer.run(mypose,movemap,sfxn,min_opts)
 
@@ -63,13 +65,48 @@ for i in range(50):
 
     mypose.set_phi(randres, orig_phi + phi_pert)
     mypose.set_psi(randres, orig_psi + psi_pert)
+    
     accepted_mc = monte_carlo_initial.boltzmann(mypose)
+    print(f'The Monte Carlo Acceptance is {accepted_mc}')
+
+
+    if accepted_mc == True:
+        counter_True += 1
+    elif accepted_mc == False:
+        counter_False += 1
+
+    if (i+1) % 10 == 0:
+        accep_rate = counter_True/10
+        print(f'The acceptance rate is{accep_rate}')
+        avg_energy = mypose.energies().total_energy()/10
+        print(f'The average evergy is{avg_energy}')
+    else:
+        continue 
 
     print(f"The lowest score is",monte_carlo_initial.lowest_score())
     monte_carlo_initial.lowest_score_pose().dump_pdb('Monte_Carlo.pdb')
 
 
-
-
-
-
+print(f'The amount of accepted Monte Carlo Structure is{counter_True}')
+print(f'The amount of rejected Monte Carlo Structure is{counter_False}')
+'''
+def identify_secondary_structure_spans(ss):
+    blocks = []
+    current = None
+    for i,j in enumerate(ss, start = 1):
+        if j in ('E','H'):
+            if current is None:
+                current = j
+                start = i
+            elif j != current:
+                blocks.append((start, i-1))
+                current = j
+                start = i
+        else:
+            if current is not None:
+                blocks.append((start, i-1))
+                current = None
+                start = None
+    if current is not None:
+        blocks.append((start,len(ss)))
+    return blocks
